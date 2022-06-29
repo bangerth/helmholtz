b362a10f7e56f82baa6f0f003012fd0a9a42ec64
Wolfgang Bangerth <bangerth@colostate.edu>
2022-06-29 15:26:27 -0600

    Also plot the solution.

--- geometries/cylinder-from-dealii/solution.py
+++ geometries/cylinder-from-dealii/solution.py
@@ -2,19 +2,23 @@
 # arbitrary points along the x-axis.
 
 import cmath
+import numpy as np
+import matplotlib.pyplot as plt
 
 # For the case with no attenuation
-c=343.287
-rho=1.205728
+#c=343.287
+#rho=1.205728
 
 # For the case with attenuation
-#c=complex(291.437,47.84)
-#rho=complex(1.5845,-0.3942)
+c=complex(291.437,47.84)
+rho=complex(1.32164,-0.163955)
+B=complex(134606.,6726.26)
+c=cmath.sqrt(B/rho)
 
 
-
-L=0.004
-omega=2*3.1415926*100000
+L=0.008
+f=10000
+omega=2*3.1415926*f
 j = complex(0,1)
 
 
@@ -25,7 +29,22 @@ def u(x) :
     return -1/(rho*c) * (cmath.exp(j*omega/c*x) + cmath.exp(j*omega/c*(2*L-x)) ) / ( 1-cmath.exp(2*j*omega/c*L) )
 
 
+
+
 # The evaluation point chosen in the input file:
 x = 0.003
-print "Pressure at ", x, ": ", p(x)
-print "Velocity at ", x, ": ", u(x)
+print ("Pressure at ", x, ": ", p(x))
+print ("Velocity at ", x, ": ", u(x))
+
+
+# Finally also plot the solution:
+x = np.arange(0, L, L/1000)
+plt.figure()
+plt.ylabel('p(x)')
+y = np.array(x,dtype=complex)
+for i in range(0,x.size) :
+  y[i] = complex(p(x[i]))
+plt.plot(x, np.real(y), '-', label="Real part of p(x)")
+plt.plot(x, np.imag(y), '-', label="Imaginary part of p(x)")
+plt.legend()
+plt.show()
